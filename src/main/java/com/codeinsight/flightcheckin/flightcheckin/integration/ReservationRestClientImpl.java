3b0f6f6cc92b1af7873a9ad7e951ef08b033762f
Veselin Sevo <veselinsevo@gmail.com>
2022-05-21 00:59:49 +0200

    Comments added

--- src/main/java/com/codeinsight/flightcheckin/flightcheckin/integration/ReservationRestClientImpl.java
+++ src/main/java/com/codeinsight/flightcheckin/flightcheckin/integration/ReservationRestClientImpl.java
@@ -10,12 +10,14 @@ public class ReservationRestClientImpl implements ReservationRestClient {
 
     public static final String RESERVATION_REST_URL = "http://localhost:8080/flightreservation/reservations/";
 
+    //Sends a request(part of request is id in this case which we got from controller) to server's RestController and expects return of Reservation object
     @Override
     public Reservation findReservation(Long id) {
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(RESERVATION_REST_URL+id, Reservation.class);
     }
 
+    //Sends a request(part of request is ReservationUpdateRequest in this case which we got from controller) to server's RestController and expects return of Reservation object
     @Override
     public Reservation updateReservation(ReservationUpdateRequest request) {
         RestTemplate restTemplate = new RestTemplate();
